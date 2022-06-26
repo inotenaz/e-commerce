d77607dd1ff76894c2719262beccf90900d23b7e
inotenaz <inotenaz@gmail.com>
2022-06-26 17:15:33 -0300

    correcao teste heroku 5

--- src/main/java/com/inotenaz/ecommerce/resources/ClienteResource.java
+++ src/main/java/com/inotenaz/ecommerce/resources/ClienteResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.inotenaz.ecommerce.domain.Cliente;
 import com.inotenaz.ecommerce.services.ClienteService;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping(value="/clientes")
 public class ClienteResource {
